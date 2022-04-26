8976df450424115aa189b294a8eb3eadac198689
Etienne Ferrandi <72975677+EtienneFerrandi@users.noreply.github.com>
2022-04-26 10:22:48 +0200

    add R code

---
+++ test.R
@@ -0,0 +1,29 @@
+library(dplyr)
+library(tidyverse)
+library(sf)
+library(mapview)
+library(leaflet)
+library(leafem)
+library(sp)
+
+
+tab=read.csv(file = "~/sermons_hors_Hippo.csv")
+
+#projection of GPS coordinates on a world map
+position=data.frame(tab$numéro,tab$longitude,tab$latitude)
+map=as_tibble(position)
+map=map%>% drop_na() 
+locations=st_as_sf(map, coords = c('tab.longitude', 'tab.latitude'), crs = 4326)
+locations=st_jitter(locations,factor = 0.001) #pour éviter la superposition des points, ce qui rendrait la carte illisible
+
+x=mapview(locations) %>%
+  addStaticLabels(label = locations$tab.numéro,
+                  noHide = TRUE,
+                  direction = 'top',
+                  textOnly = TRUE,
+                  textsize = "20px")
+  
+
+
+mapshot(x, file = paste0(getwd(),"/file.png")) #save
+
